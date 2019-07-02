205d58676ce22ed474557e4385102aa13703eaa1
luoboguapo <760106387@qq.com>
2019-07-02 14:53:25 +0800

    12

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        //spring 应用启动1
+        SpringApplication.run(DemoApplication.class, args);
+
+    }
+
+}
